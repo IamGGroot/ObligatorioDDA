2e4dec6e4e43860d1d5f2a8d747c123b2b8c617b
analopez2 <anaplopez07@gmail.com>
2023-05-10 21:14:57 -0300

    Cambios Santiago-1

--- ObligatorioDDA/src/Dominio/Propietario.java
+++ ObligatorioDDA/src/Dominio/Propietario.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Dominio;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author imm
- */
+
 public class Propietario extends Usuario {
     
    private double saldoMinimo;
--- ObligatorioDDA/src/Dominio/Sesion.java
+++ ObligatorioDDA/src/Dominio/Sesion.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Dominio;
 
 import java.util.Date;
 
-/**
- *
- * @author imm
- */
+
 public class Sesion {
     
     private Usuario usuario;
