454a7880bf8d854334a48e894570ed51e9cdb9e1
Gerardo Silva <59371283+IamGGroot@users.noreply.github.com>
2023-05-21 17:51:03 -0300

    creadas todos las interfaces swing de los CU

--- ObligatorioDDA/src/Main/Main.java
+++ ObligatorioDDA/src/Main/Main.java
@@ -1,10 +1,19 @@
 package Main;
 
+import InterfazPropietarios.IngresarALaAplicacion;
+import InterfazPropietarios.TableroDeControlDelPropietario;
 
 public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
+
+        IngresarALaAplicacion ventana = new IngresarALaAplicacion();
+
+        TableroDeControlDelPropietario ventana2 = new TableroDeControlDelPropietario();
+
+        ventana.setVisible(true);
+        ventana2.setVisible(true);
     }
 
 }
