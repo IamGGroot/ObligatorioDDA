d97cf9aacf8298dfb0649dcff3f89bf2d99bf5fc
Santi <imm@939208W10>
2023-06-08 18:37:51 -0300

    Santi Cambios

--- ObligatorioDDA/src/Controlador/ControladorEmularAprobacionRecarga.java
+++ ObligatorioDDA/src/Controlador/ControladorEmularAprobacionRecarga.java
@@ -2,9 +2,11 @@ package Controlador;
 
 import Dominio.Administrador;
 import Interfaz.VistaEmularAprobacionRecarga;
+import Observer.Observable;
+import Observer.Observador;
 import Servicios.FachadaServicios;
 
-public class ControladorEmularAprobacionRecarga {
+public class ControladorEmularAprobacionRecarga implements Observador{
 
     private VistaEmularAprobacionRecarga vista;
     private Administrador usuarioAdmin;
@@ -18,7 +20,18 @@ public class ControladorEmularAprobacionRecarga {
         this.vista.mostrarRecargas(FachadaServicios.getInstancia().getPropietariosConRecargasPendientes());
     }
 
+    @Override
+    public void notificar(Observable origen, Object evento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
+    public void cerrar(){
+    if(vista.confirmar("Confirma que desea salir?", "Salir del sistema")){
+     //Desuscribir
+        vista.salir();
+    }
+    }
+     
 
 
 }
